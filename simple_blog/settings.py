9717e5499298b5797631bbf0cc0401728744cd1d
hengage <tunecarterkillz@gmail.com>
2021-11-02 00:07:42 +0100

    set up decouple and the environment variables settings

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from pathlib import Path
-from django.contrib.auth.hashers import PBKDF2PasswordHasher
+
+from decouple import config, Csv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR,'apps'))
@@ -14,12 +15,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b#4q5s)i(eh4gld6$ez_xo5v+-f2a#c)11a(%itsa!3p+&!_ya'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']#['local']
+# ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']#['local']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 INSTALLED_APPS = [
