3bba17ebb8327bf4920039e5750e189800c52b16
hengage <tunecarterkillz@gmail.com>
2021-11-03 13:53:23 +0100

    changed debug mode

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG',  default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']#['local']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
