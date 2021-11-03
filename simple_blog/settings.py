efd61657bbae3d63fa375c3b94f9f17be0506c7e
hengage <tunecarterkillz@gmail.com>
2021-11-03 17:11:25 +0100

    removed the if statement from previous commit

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -20,8 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',  default=True, cast=bool)
-if DEBUG == False:
-    ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
