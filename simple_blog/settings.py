1bf2587658373ee5536fd066de42459e549a8943
hengage <tunecarterkillz@gmail.com>
2021-11-03 18:19:27 +0100

    set debug to true in settings

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',  default=False, cast=bool)
+DEBUG = config('DEBUG',  default=True, cast=bool)
 
 ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
