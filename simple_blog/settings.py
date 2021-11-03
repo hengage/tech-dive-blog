ec35f9c826c61cc019776579d74c9b629323de94
hengage <tunecarterkillz@gmail.com>
2021-11-03 18:15:10 +0100

    still trying to set the debug for production

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',  default=False, cast=bool)
-if DEBUG == False:
-    ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
