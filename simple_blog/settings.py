df1283c8e78cfb416fb363ff79dda62775bf754b
hengage <tunecarterkillz@gmail.com>
2021-11-04 02:04:01 +0100

    set if statement for debug and allowed hosts

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -21,8 +21,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',  default=False, cast=bool)
 # DEBUG = (config('DEBUG') =='True')
-
-ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
+if DEBUG == False:
+    ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
