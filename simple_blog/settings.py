b7212218c06e640ead2505ab003cd0a990750094
hengage <tunecarterkillz@gmail.com>
2021-12-02 02:14:58 +0100

    Deleted 'pygments.css' and created 'markdownx.css' in its place to solve unicode error

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['softdevblog.herokuapp.com', 'localhost', '127.0.0.1']
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
