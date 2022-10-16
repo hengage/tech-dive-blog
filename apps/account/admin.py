68487d846fa5f4916319844dc59663ce6c36d54e
hengage <tunecarterkillz@gmail.com>
2022-10-16 03:41:05 +0100

    Feature: user admin customization

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,22 +1,82 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+
 from .forms import CustomUserCreationForm #, UserChangeForm
 
+User = get_user_model()
 
-
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+    model = User
     add_form = CustomUserCreationForm
     #form = UserChangeForm
-    
-    list_display = ['email', 'username', 'first_name', 'last_name',  'is_staff','date_registered' ]
 
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('first_name', 'last_name', 'email',)}),
+    list_display = [
+        'email', 'last_name', 'is_staff',
+    ]
+
+    fieldsets= [
+        ("Basic User Details", {
+            "fields": ['email', 'first_name', 'last_name',]
+        }),
+
+        (
+            "User Status (sensitive area, proceed with caution)",
+            {
+                "fields": [
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ],
+                "classes": ["collapse"],
+            },
+        ),
+
+        (
+            'Important Dates',
+            {
+                "fields": ["date_joined", "last_login"]
+            },
+        ),
+
+        ("Groups And Permissions", 
+            {
+                "fields": ["groups", "user_permissions"], 
+                "classes": ["collapse"]
+            }
+        ),
+    ]
+
+    add_fieldsets = (
+        (
+            "User Personal Details",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                ),
+            },
+        ),
+
+        (
+            "User status",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "is_active"
+                ),
+            },
+        ),
     )
+    
+    
+    
+    ordering = ['email']
 
-
-admin.site.register(CustomUser, CustomUserAdmin)
-
-
+    # add_fieldsets = UserAdmin.add_fieldsets + (
+    #     (None, {'fields': ('first_name', 'last_name', 'email',)}),
+    # )
