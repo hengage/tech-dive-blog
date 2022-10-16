b77cfbcdfd147ae598881aa0c2982143aff8a75c
hengage <tunecarterkillz@gmail.com>
2022-10-16 02:26:12 +0100

    Refactor: delete reference to username field
    change 'password1' and 'password2' to 'password', removing the confirm passwword field in account form

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -4,26 +4,20 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
-    # password1 and password2 are "custom" fields on the UserCreationForm since they do not exist as model fields on the User model. Meta.widgets will not work for these custom fields, so I redefined their fields and their widgets here.
-    password1 = forms.CharField(
+    # password is a "custom" field on the UserCreationForm, since it does not exist as a model field on the User model. 
+    # Meta.widgets will not work for this custom field, so I redefined it's field and widget here.
+    password = forms.CharField(
         label='Password',
         widget=forms.PasswordInput(attrs={'class':'form-control',})
     )
-
-    password2 = forms.CharField(
-        label='Confirm password',
-        widget=forms.PasswordInput(attrs={'class':'form-control',})
-    )
             
-
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'email', ]
+        fields = ['first_name', 'last_name', 'email', ]
 
         widgets = {
             'first_name': forms.TextInput(attrs={'class':'form-control'}),
             'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'username': forms.TextInput(attrs={'class':'form-control'}),
             'email': forms.EmailInput(attrs={'class':'form-control'}),
         }
 
@@ -32,8 +26,6 @@ class CustomUserCreationForm(UserCreationForm):
         }
 
     field_order = [
-        'first_name',  'last_name', 
-        'username','email',   
-        'password1', 'password2',
+        'first_name',  'last_name', 'email',  'password',
     ]
         
