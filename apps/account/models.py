69295f80ecbf69caff48f416b8abd58c228791c8
hengage <tunecarterkillz@gmail.com>
2022-10-16 02:17:55 +0100

    Feature: customuser model with abstractbaseduser

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,7 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-class CustomUser(AbstractUser):
-    date_registered = models.DateTimeField(auto_now_add=True)
 
-    #id_photo = models.ImageField()
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    class Meta:
+        ordering = ("email",)
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return f"{self.first_name.title()} {self.last_name.title()} - {self.email}"
