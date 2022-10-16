2bc2a48499272f7b9c2a0fdf59ce118ff98e8380
hengage <tunecarterkillz@gmail.com>
2022-10-16 05:25:50 +0100

    Refactor: refactored templates

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -6,26 +6,35 @@ class CustomUserCreationForm(UserCreationForm):
 
     # password is a "custom" field on the UserCreationForm, since it does not exist as a model field on the User model. 
     # Meta.widgets will not work for this custom field, so I redefined it's field and widget here.
-    password = forms.CharField(
+    password1 = forms.CharField(
         label='Password',
-        widget=forms.PasswordInput(attrs={'class':'form-control',})
+        widget=forms.PasswordInput(attrs={'class':'form-control'})
     )
+    password2 = None
             
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'email', ]
+        fields = ['first_name', 'last_name', 'email', 'password1']
 
         widgets = {
-            'first_name': forms.TextInput(attrs={'class':'form-control'}),
-            'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={
+                'class':'form-control', 
+                }),
+            
+            'last_name': forms.TextInput(attrs={
+                'class':'form-control'
+                }),
+                
+            'email': forms.EmailInput(attrs={
+                'class':'form-control'
+                }),
         }
 
-        help_texts = {
-            'username': None,
-        }
+        # help_texts = {
+        #     'first_name': 'Your first name',
+        # }
 
-    field_order = [
-        'first_name',  'last_name', 'email',  'password',
-    ]
+    # field_order = [
+    #     'first_name',  'last_name', 'email',  'password1',
+    # ]
         
