40147d9614dfc679f8c581e8fc056adb6c8a7099
hengage <tunecarterkillz@gmail.com>
2022-11-02 05:27:27 +0100

    Feature: config tailwind content with template paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/*.html'
+    './templates/*.html',
+    'apps/*/templates/*/*.html',
+    'apps/*/templates/*.html'
   ],
   theme: {
     extend: {},
