b6eba7e4a10dd36ab2ccd5474e11873087b9dd31
hengage <tunecarterkillz@gmail.com>
2022-11-06 01:24:21 +0100

    Style: hover styles on buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
     extend: {
       colors: {
         'primary-color': '#0b1b4b',
-        'primary-color2':'#3561eb',
+        'primary-color2':'#FB9129',
+        'button-hover':'#0e0761',
         'sec-color': 'e78e26',
         'pry-font-color': '#948894',
         'heading-color': '#6b6a94'
