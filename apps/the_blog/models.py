4121d61b44ebe1ab02c530fcc755e21d12c55888
hengage <tunecarterkillz@gmail.com>
2022-11-08 23:13:30 +0100

    Feature: get_absolute_url method on Category model

--- apps/the_blog/models.py
+++ apps/the_blog/models.py
@@ -16,6 +16,9 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.category_name}"
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug':self.slug})
     class Meta: 
         verbose_name = 'category'
         verbose_name_plural = 'categories'
