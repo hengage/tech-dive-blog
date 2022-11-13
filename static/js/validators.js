5e799f26f8ee356e483101743f16ad7e4877fd7c
hengage <tunecarterkillz@gmail.com>
2022-11-13 18:20:31 +0100

    Feature: validate signup form
    Check if fields are empty, display response

--- static/js/validators.js
+++ static/js/validators.js
@@ -1,32 +1,53 @@
 "use strict";
 
-const validate = (error) => {
-    const signupForm = document.getElementById("signup_form");
-    const signupFirstName = signupForm.first_name
-    const signupLastName = signupForm.last_name
-    const signupPassword = signupForm.password1
+const signupForm = document.getElementById("signup_form");
+const signupFirstName = signupForm.first_name
+const signupLastName = signupForm.last_name
+const signupPassword = signupForm.password1
+const signupEmail = signupForm.email
+console.log({signupEmail})
 
-    const signupFormFirstNameError = document.getElementById("signup-firstname-error");
+
+const validate = (error) => {
+    const signupFormFirstNameError = document.getElementById("signup-firstname-error-text");
     if ( signupFirstName.value.includes(' ') ) {
         error.push('First name should not contain spaces')
         signupFormFirstNameError.textContent = 'First name should not contain whitespace'
-    }   else {
+    }  else if ( signupFirstName.value === '' ) {
+        error.push('First name is required')
+        signupFormFirstNameError.textContent = 'First name is required' 
+    } else {
         signupFormFirstNameError.textContent = ''
     } 
 
-    const signupFormLastNameError = document.getElementById("signup-lastname-error");
+    const signupFormLastNameError = document.getElementById("signup-lastname-error-text");
     if ( signupLastName.value.includes(' ') ) {
         error.push('Last name should not contain spaces')
         signupFormLastNameError.textContent = 'Last name should not contain whitespace'
+    } else if ( signupLastName.value === '' ) {
+        error.push('Last name is required')
+        signupFormLastNameError.textContent = 'Last name is required'
     } else {
         signupFormLastNameError.textContent = ''
     }
 
-    const passwordError =  document.getElementById('signup-password-error')
+    const signupEmailError = document.getElementById('signup-email-error-text')
+    if ( signupEmail.value === '') {
+        error.push('Email is required')
+        signupEmailError.textContent = 'Email is required'
+    } else {
+        signupEmailError.textContent = ''
+    }
+
+
+    const passwordError =  document.getElementById('signup-password-error-text')
     if ( signupPassword.value.includes(' ') ) {
         error.push('Password cannot contain spaces')
         passwordError.textContent = 'Password cannot contain whitespace'
-    }   else {
+    } else  if ( signupPassword.value === '' ) {
+        error.push('Psassword is required')
+        passwordError.textContent = 'Password is required'
+    } else {
         passwordError.textContent = ''
     }
 }
