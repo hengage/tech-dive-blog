8dc8e6b1070883dcaadd336cd4db022dfe6143d5
hengage <tunecarterkillz@gmail.com>
2022-11-16 06:20:29 +0100

    urls path changed and improved on

--- apps/the_blog/urls.py
+++ apps/the_blog/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home' ),
     path('article/<slug:slug>', views.PostDetailView, name='article_detail'),
-    path('article/<slug:slug>/delete_post/', views.DeletePostView.as_view(), name='delete_post'),
-    path('article/edit_post/<slug:slug>/', views.UpdatePostView.as_view(), name='edit_post'),
-    path('create_post/', views.AddPostView.as_view(), name='create_post'),
+    path('article/<slug:slug>/delete/', views.DeletePostView.as_view(), name='delete_post'),
+    path('article/edit/<slug:slug>/', views.UpdatePostView.as_view(), name='edit_post'),
+    path('article/new/', views.AddPostView.as_view(), name='create_post'),
     path('search/', views.SearchPostsResultListView.as_view(), name='posts_search_results'),
     path('category/<slug:slug>', views.CategoryDetailView.as_view(), name='category')
 ]
