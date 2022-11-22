d7f15b793f6a304dfec82db1ced956b6c65989ee
hengage <tunecarterkillz@gmail.com>
2022-11-22 02:39:31 +0100

    Style: delete post form modal

--- static/js/main.js
+++ static/js/main.js
@@ -3,10 +3,10 @@
 
 // Delete post using modal on the same page
 const deletePost = document.getElementById("delete-post");
+console.log(deletePost.href)
 
 deletePost?.addEventListener("click", () => {
     document.getElementById("delete-post-form").action = deletePost.href;
-    console.log('yeahh')
 });
 
 
