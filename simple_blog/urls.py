5b3db3e2a42da57894f697eb0417608391dce9d5
hengage <tunecarterkillz@gmail.com>
2022-11-25 00:49:29 +0100

    Fixed: markdownx url import error

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static 
 
-from markdownx import urls as markdownx
+# from markdownx import urls as markdownx
 
 urlpatterns = [
     path('admin/', admin.site.urls),
